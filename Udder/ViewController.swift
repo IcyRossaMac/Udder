fc831439bf0e2a441356c6920667ed37f661bfa6
Rossa McLaughlin <IcyRossa@gmail.com>
2017-11-14 11:50:33 +0000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Rossa McLaughlin on 14/11/2017.
+//  Copyright © 2017 Rossa McLaughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
